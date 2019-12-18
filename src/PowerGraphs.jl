1f538ec9fff68588a689556720be53ee72a3c2e9
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2019-12-18 10:35:03 +0100

    First working test

--- src/PowerGraphs.jl
+++ src/PowerGraphs.jl
@@ -4,4 +4,6 @@ include("power_graph_types.jl")
 include("models_to_graph.jl")
 include("radial_grid_operations.jl")
 
+export RadialPowerGraph
+
 end # module
--- test/test_radial_power_graph.jl
+++ test/test_radial_power_graph.jl
@@ -3,7 +3,7 @@ using Test
 using LightGraphs
 
 # Create the graph we should end up with
-G = Graph(5)
+G = DiGraph(5)
 add_edge!(G, 1, 2)
 add_edge!(G, 2, 3)
 add_edge!(G, 3, 4)
