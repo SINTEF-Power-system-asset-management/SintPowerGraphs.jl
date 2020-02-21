35b9d1ba50c51b1d979d83b486380063258ecff9
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2020-02-21 10:33:04 +0100

    Added method for getting incidence matrix

--- src/PowerGraphs.jl
+++ src/PowerGraphs.jl
@@ -10,7 +10,7 @@ include("radial_grid_operations.jl")
 export merge_line_segments, directed_from_feeder, remove_zero_impedance_lines, remove_low_impedance_lines
 
 include("power_graph_properties.jl")
-export get_bus_data, get_branch_data, is_load_bus, is_gen_bus, set_branch_data!, set_bus_data!, get_π_equivalent, get_dc_admittance_matrix, get_power_injection_vector, get_susceptance_vector
+export get_bus_data, get_branch_data, is_load_bus, is_gen_bus, set_branch_data!, set_bus_data!, get_π_equivalent, get_dc_admittance_matrix, get_power_injection_vector, get_susceptance_vector, get_incidence_matrix
 
 include("circuit_operations.jl")
 export π_segment, is_zero_impedance_line, series_impedance_norm
