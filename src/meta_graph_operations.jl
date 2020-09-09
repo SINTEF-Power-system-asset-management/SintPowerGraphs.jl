870fd7d2345baa22c2f3b978a9628631b69836ef
Raskospoon <Michele.Garau@sintef.no>
2020-09-09 12:19:37 +0200

    Bugfix: removed open switch edges before BFS

--- src/meta_graph_operations.jl
+++ src/meta_graph_operations.jl
@@ -41,13 +41,14 @@ ordering from :args in the edges is not used...yet)
 """
 function traverse(g::MetaGraph, start::Int = 0, dfs::Bool = true)::Vector{Int}
     # start = start == 0 ? root(g) : start
+    
     seen = Vector{Int}()
     visit = Vector{Int}([start])
-    # @assert start in vertices(g) "can't access $start in $(props(g, 1))"
+    @assert start in vertices(g_copy) "can't access $start in $(props(g, 1))"
     while !isempty(visit)
         next = pop!(visit)
         if !(next in seen)
-            for n in neighbors(g, next)
+            for n in neighbors(g_copy, next)
                 if !(n in seen)
                     if dfs append!(visit, n) else insert!(visit, 1, n) end
                 end
@@ -62,7 +63,14 @@ end
 function subgraph(g::MetaGraph, start::Int = 0)::MetaDiGraph
     # start = start == 0 ? root(g) : start
 
-    inds = traverse(g, start, false)
+    # removing open switch edges before traversing with BFS
+    g_copy = copy(g)
+    open_switches_iter = filter_edges(network.meta, (g,x)->(get_prop(g, x, :switch) == 0))
+    for e in open_switches_iter
+        rem_edge!(g_copy, e)
+    end
+
+    inds = traverse(g_copy, start, false)
     newgraph = MetaDiGraph()
     set_indexing_prop!(newgraph, :name)
     set_prop!(newgraph, :root, 1)
@@ -70,18 +78,18 @@ function subgraph(g::MetaGraph, start::Int = 0)::MetaDiGraph
 
     for (newi, i) in enumerate(inds)  # in BFS order
         add_vertex!(newgraph)
-        set_prop!(newgraph, newi, :name, get_prop(g, i, :name))
+        set_prop!(newgraph, newi, :name, get_prop(g_copy, i, :name))
         reindex[i] = newi
     end
 
-    for e in edges(g)
-        # I sort src and tar for keeping the order of discovery in DFS
+    for e in edges(g_copy)
+        # I sort src and tar for keeping the order of discovery in BFS
         src, tar = sort([get(reindex, e.src, nothing), get(reindex, e.dst, nothing)])
         if !(nothing in [src, tar])
-            if get_prop(g, e, :switch) != 0 # I add the edge only if switches are closed (original configuration analyzed)
-                add_edge!(newgraph, src, tar)
-                set_prop!(newgraph, src, tar, :switch, get_prop(g, e.src, e.dst, :switch))
-            end
+            #if get_prop(g, e, :switch) != 0 
+            add_edge!(newgraph, src, tar)
+            set_prop!(newgraph, src, tar, :switch, get_prop(g_copy, e.src, e.dst, :switch))
+           # end
         end
     end
     return newgraph
