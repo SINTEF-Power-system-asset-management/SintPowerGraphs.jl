b44f9b272df377da0a6b13ffd37cbc2a465a6490
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2021-02-09 22:26:06 +0100

    Added support for putting all branch stuff in one file
    I added a method for putting switches, indicators, etc.
    in one dataframe together with the branches.
    
    On GH-24

--- examples/merge_east_side.jl
+++ examples/merge_east_side.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.4
+# v0.12.20
 
 using Markdown
 using InteractiveUtils
@@ -30,9 +30,10 @@ md"
 
 # ╔═╡ b5d5c9c0-6a10-11eb-28fa-2747e5ca8119
 begin
-	fname = "lionshield.toml"
+	fname = "east_side.toml"
 	filepath = joinpath(@__DIR__, joinpath("cases", fname))
 	example = RadialPowerGraph(filepath)
+	example.mpc
 end
 
 # ╔═╡ cf309260-6a10-11eb-12ae-61bbfb78b061
@@ -59,9 +60,61 @@ begin
 										:failure_frequency_temporary => 0.0,
 										:length => 0.0))
 	red_example = merge_line_segments(example, aggregators=aggregators)
-
-	red_example.ref_bus
+	red_example.mpc
 end
+
+# ╔═╡ c1a5ed9e-6a35-11eb-2455-03bc10cca19e
+md"
+Before merging we had 1133 buses in the case, now we have only 440 buses. We can also check how many lines shorter than 2cm we have now:"
+
+# ╔═╡ daac54b0-6a35-11eb-34ec-7116d9268a29
+begin
+	df_short = red_example.mpc.reldata
+	# Divide by 1000 to go from km to m
+	df_short.length/1000
+	df_short[df_short.length.<2/100,:][:,[:f_bus, :t_bus, :length]]
+end
+
+# ╔═╡ f1dc2700-6a35-11eb-0ae2-1dad05882229
+md"
+There is now 202 lines shorter than 2cm. This is a big change compare to before when the number was 940. To be able to run power flows we will also remove lines with a low impedance. "
+
+# ╔═╡ 02d9ab90-6a36-11eb-154a-0d3a1e15d9bd
+md"
+## Removing low impedance lines
+We wil now remove low impedance lines. At the moment this functionality does not keep information about switches, fault indicators or failure rates. It merely takes the everyting connected to one end of the low impedance line and connects it to the other end. Afterwards the line and the empty end are deleted."
+
+# ╔═╡ 1ed1b450-6a36-11eb-1853-73ed3ad9c1b9
+small = remove_low_impedance_lines(red_example, 1e-5)
+
+# ╔═╡ 22c5d1e0-6a36-11eb-011c-038bd7c4fd5f
+md"
+## Plot the network
+We will now plot the network, to check that it looks reasonable. There are two nice options for plotting. We can use GraphPlots as demonstrated below."
+
+# ╔═╡ 5cced850-6a36-11eb-18ac-fbb43a74f1a3
+begin
+	set_default_plot_size(25cm, 25cm)
+	gplot(small.G, nodelabel=1:n_vertices(small), arrowlengthfrac=0)
+end
+
+# ╔═╡ 68e70180-6a36-11eb-27d6-6bbbbd43d1af
+md"
+Another nice option for plotting is to use Plots and GraphRecipes."
+
+# ╔═╡ 6efcc690-6a36-11eb-0ba8-e5b38f41b6ba
+begin
+	plotly(alpha=1, size=(700,800), dpi=150)
+	graphplot(small.G, method=:tree, nodeshape=:circle, names=1:n_vertices(small), curves=false, fontsize=5, self_edge_size=0.5)
+end
+
+# ╔═╡ f665bbe0-6a37-11eb-2308-a3737ac2784b
+md"
+## Write to matpower
+We can now write the network to csv files for later processing in MATLAB"
+
+# ╔═╡ 66802f20-6a40-11eb-374f-53db9f556ab1
+to_csv(small.mpc, joinpath(@__DIR__, "reduced_matpower"))
 
 # ╔═╡ Cell order:
 # ╠═c209f8b2-6a0b-11eb-1fa1-e92435e9f9e4
@@ -73,3 +126,14 @@ end
 # ╠═f3d389fe-6a10-11eb-05ad-2fbf6db78c4b
 # ╟─0a9ad680-6a11-11eb-2a5b-710a1f821f00
 # ╠═18c0d110-6a11-11eb-1e96-e34c52473e47
+# ╟─c1a5ed9e-6a35-11eb-2455-03bc10cca19e
+# ╠═daac54b0-6a35-11eb-34ec-7116d9268a29
+# ╟─f1dc2700-6a35-11eb-0ae2-1dad05882229
+# ╟─02d9ab90-6a36-11eb-154a-0d3a1e15d9bd
+# ╠═1ed1b450-6a36-11eb-1853-73ed3ad9c1b9
+# ╟─22c5d1e0-6a36-11eb-011c-038bd7c4fd5f
+# ╠═5cced850-6a36-11eb-18ac-fbb43a74f1a3
+# ╟─68e70180-6a36-11eb-27d6-6bbbbd43d1af
+# ╠═6efcc690-6a36-11eb-0ba8-e5b38f41b6ba
+# ╟─f665bbe0-6a37-11eb-2308-a3737ac2784b
+# ╠═66802f20-6a40-11eb-374f-53db9f556ab1
--- src/radial_grid_operations.jl
+++ src/radial_grid_operations.jl
@@ -11,26 +11,26 @@
 		- `aggregators`::Dict{Symbol, Dict{Strig, Any}}=nothing: Sum up columns of the field Symbol given by String.
 """
 function merge_line_segments(network::RadialPowerGraph;
-							keep_switches::Bool=true,
-							keep_indicators::Bool=true,
-							keep_loaddata::Bool=true,
-							keep_transformers::Bool=true,
-							aggregators::Dict{Symbol, Dict{Symbol, Float64}}=Dict{Symbol, Dict{Symbol, Float64}}())
-	red_net = RadialPowerGraph()
+                             keep_switches::Bool=true,
+                             keep_indicators::Bool=true,
+                             keep_loaddata::Bool=true,
+                             keep_transformers::Bool=true,
+                             aggregators::Dict{Symbol, Dict{Symbol, Float64}}=Dict{Symbol, Dict{Symbol, Float64}}())
+    red_net = RadialPowerGraph()
     set_indexing_prop!(red_net.G, :name)
 	
     # Set other mpc struct things
 
     # Keep track of the mapping between new and old bus numbers
-	vertices = traverse(MetaGraph(network.G), network.G[network.ref_bus, :name], true)
+    vertices = traverse(MetaGraph(network.G), network.G[network.ref_bus, :name], true)
     from_bus_int = vertices[1]
-	#TODO: This hack should not be needed.
-	network.radial = MetaDiGraph(dfs_tree(MetaGraph(network.G), network.G[network.ref_bus, :name]))
+    #TODO: This hack should not be needed.
+    network.radial = MetaDiGraph(dfs_tree(MetaGraph(network.G), network.G[network.ref_bus, :name]))
 
     # Add the reference bus to network
     temp = get_bus_data(network, network.ref_bus)
     push_bus!(red_net, temp)
-	red_net.ref_bus = network.ref_bus
+    red_net.ref_bus = network.ref_bus
 
     red_net.mpc.baseMVA = network.mpc.baseMVA
    
@@ -59,66 +59,64 @@ function merge_line_segments(network::RadialPowerGraph;
                     go_back += 1
                 end
             end
-		end
-		# Fix mapping
-		from_bus = network.G[from_bus_int, :name]
-		f_bus = network.G[f_bus_int, :name]
-		t_bus = network.G[t_bus_int, :name]
+        end
+        # Fix mapping
+        from_bus = network.G[from_bus_int, :name]
+        f_bus = network.G[f_bus_int, :name]
+        t_bus = network.G[t_bus_int, :name]
 
         neighbor_count = length(neighbors(network.radial, t_bus_int))
         # Check if the we have reached a load, generator, intersection or end of radial
 		
-		# A bit sneaky, but I change t_bus and f_bus to the id
-		if is_gen_bus(network, t_bus) || is_load_bus(network, t_bus) || neighbor_count != 1 || (keep_switches && is_switch(network, f_bus, t_bus)) || (keep_indicators && is_indicator(network, f_bus, t_bus)) || (keep_switches && is_neighbor_switch(network, f_bus, t_bus)) || (keep_indicators && is_neighbor_indicator(network, f_bus, t_bus)) || (keep_transformers && is_transformer(network, f_bus, t_bus))
+        # A bit sneaky, but I change t_bus and f_bus to the id
+        if is_gen_bus(network, t_bus) || is_load_bus(network, t_bus) || neighbor_count != 1 || (keep_switches && is_switch(network, f_bus, t_bus)) || (keep_indicators && is_indicator(network, f_bus, t_bus)) || (keep_switches && is_neighbor_switch(network, f_bus, t_bus)) || (keep_indicators && is_neighbor_indicator(network, f_bus, t_bus)) || (keep_transformers && is_transformer(network, f_bus, t_bus))
             # add the bus and the line
-			# get the bus we are adding
-			new_bus = get_bus_data(network, t_bus)
+            # get the bus we are adding
+            new_bus = get_bus_data(network, t_bus)
             push_bus!(red_net, new_bus)
 			
-			if keep_loaddata && is_load_bus(network, t_bus)
-				push_loaddata!(red_net, get_loaddata(network, t_bus), new_bus.ID)
-			end
-            
+            if keep_loaddata && is_load_bus(network, t_bus)
+                push_loaddata!(red_net, get_loaddata(network, t_bus), new_bus.ID)
+            end
             # This can probably be fixed more elegantly using metaprogramming
             if is_gen_bus(network, t_bus)
-				temp = deepcopy(get_gen_data(network, t_bus))
+                temp = deepcopy(get_gen_data(network, t_bus))
                 push_gen!(red_net, temp, t_bus) 
             end
-			if !isempty(aggregators)
-				for (field, columns) in aggregators
-					for (column, data) in columns
-						if is_branch_type_in_graph(network, field, f_bus, t_bus)
-							agg = aggregators[field][column]+get_branch_data(network, field, column, f_bus, t_bus)
-						else
-							agg = aggregators[field][column]
-						end
-							if isempty(getfield(red_net.mpc, field)) || !is_branch_type_in_graph(red_net, field, from_bus, t_bus)
-								if is_branch_type_in_graph(network, field, f_bus, t_bus)
-								temp = deepcopy(get_branch_data(network,
-													   field,
-													   f_bus,
-													   t_bus))
-								temp[column] = agg
-									push_branch!(red_net,
-												 field,
-												 from_bus,
-												 t_bus,
-												 temp)
-								end
-							else
-								set_branch_data!(red_net,
-												field,
-												column,
-												from_bus,
-												t_bus,
-												agg)
-						end
-						aggregators[field][column] = 0.0
-
-					end
-				end
-			end
-			branch = deepcopy(get_branch_data(network, f_bus, t_bus))
+            if !isempty(aggregators)
+                for (field, columns) in aggregators
+                    for (column, data) in columns
+                        if is_branch_type_in_graph(network, field, f_bus, t_bus)
+                            agg = aggregators[field][column]+get_branch_data(network, field, column, f_bus, t_bus)
+                        else
+                            agg = aggregators[field][column]
+                        end
+                        if isempty(getfield(red_net.mpc, field)) || !is_branch_type_in_graph(red_net, field, from_bus, t_bus)
+                            if is_branch_type_in_graph(network, field, f_bus, t_bus)
+                                temp = deepcopy(get_branch_data(network,
+                                                                field,
+                                                                f_bus,
+                                                                t_bus))
+                                temp[column] = agg
+                                push_branch!(red_net,
+                                             field,
+                                             from_bus,
+                                             t_bus,
+                                             temp)
+                            end
+                        else
+                        set_branch_data!(red_net,
+                                         field,
+                                         column,
+                                         from_bus,
+                                         t_bus,
+                                         agg)
+                        end
+                        aggregators[field][column] = 0.0
+                    end
+                end
+            end
+            branch = deepcopy(get_branch_data(network, f_bus, t_bus))
             push_branch!(red_net, from_bus, t_bus, branch)
             
             π += get_π_equivalent(network, f_bus, t_bus)
@@ -128,31 +126,31 @@ function merge_line_segments(network::RadialPowerGraph;
 
             set_branch_data!(red_net, from_bus, t_bus, branch)
 
-			# Fix from and to bus for switches or fault indicators
-			if keep_switches && is_switch(network, f_bus, t_bus)
-				push_switch!(red_net,
-							 from_bus,
-							 t_bus,
-							deepcopy(get_switch_data(network,
-											f_bus,
-											t_bus)[1,:]))
-			end
-			if keep_indicators && is_indicator(network, f_bus, t_bus)
-				push_indicator!(red_net,
-								from_bus,
-								t_bus,
-								deepcopy(get_indicator_data(network,
-												f_bus,
-												t_bus)[1,:]))
-			end
-			if keep_transformers && is_transformer(network, f_bus, t_bus)
-				push_transformer!(red_net,
-								from_bus,
-								t_bus,
-								deepcopy(get_transformer_data(network,
-												f_bus,
-												t_bus)[1,:]))
-			end
+            # Fix from and to bus for switches or fault indicators
+            if keep_switches && is_switch(network, f_bus, t_bus)
+                push_switch!(red_net,
+                             from_bus,
+                             t_bus,
+                             deepcopy(get_switch_data(network,
+                                      f_bus,
+                                      t_bus)[1,:]))
+            end
+            if keep_indicators && is_indicator(network, f_bus, t_bus)
+                push_indicator!(red_net,
+                                from_bus,
+                                t_bus,
+                                deepcopy(get_indicator_data(network,
+                                         f_bus,
+                                         t_bus)[1,:]))
+            end
+            if keep_transformers && is_transformer(network, f_bus, t_bus)
+                push_transformer!(red_net,
+                                  from_bus,
+                                  t_bus,
+                                  deepcopy(get_transformer_data(network,
+                                           f_bus,
+                                           t_bus)[1,:]))
+            end
 
             for field in fieldnames(π_segment)
                 setfield!(π, field, 0)
@@ -162,16 +160,16 @@ function merge_line_segments(network::RadialPowerGraph;
                 from_bus_int = 0
             else
                 from_bus_int = t_bus_int
-			end
+            end
         else
             π += get_π_equivalent(network, f_bus, t_bus)
-			if  !isempty(aggregators)
-				for (field, columns) in aggregators
-					for (column, data) in columns
-						aggregators[field][column] += get_branch_data(network, field, column, f_bus, t_bus)
-					end
-				end
-			end
+            if  !isempty(aggregators)
+                for (field, columns) in aggregators
+                    for (column, data) in columns
+                        aggregators[field][column] += get_branch_data(network, field, column, f_bus, t_bus)
+                    end
+                end
+            end
         end
         index += 1
     end
@@ -191,18 +189,18 @@ function remove_low_impedance_lines(network_orig::PowerGraphBase, tol::Float64=0
             t_bus = branch.t_bus
             f_bus = branch.f_bus
             append!(delete_rows, getfield(branch, :row))
-			# Ensure that swing bus is connected
-			t_bus_type = network.bus[network.bus.ID .== t_bus, :type][1]
+            # Ensure that swing bus is connected
+            t_bus_type = network.bus[network.bus.ID .== t_bus, :type][1]
             if  t_bus_type == 3
                 network.bus[f_bus, :type] = 3
             end
             # Connect the buses that were disconnected
             network.branch[network.branch.f_bus .== t_bus, :f_bus] .= f_bus
             network.branch[network.branch.t_bus .== t_bus, :t_bus] .= f_bus
-           
+            
             # Move what may have been on the bus
             network.gen[network.gen.ID .== t_bus, :ID] .= f_bus
-
+            
             delete_bus!(network, t_bus)
         end
     end
