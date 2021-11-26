022404bad73bdd0cba747434be060a2f7e42c203
Sigurd Hofsmo Jakobsen <sigurd.jakobsen@sintef.no>
2021-11-26 10:11:07 +0100

    Fixed sign for load in power_injection_vector

--- src/case_format.jl
+++ src/case_format.jl
@@ -320,12 +320,12 @@ function get_power_injection_vector(case::Case)::Vector{Float64}
 	Pd = zeros(size(case.bus, 1))
     Pg = zeros(length(Pd))
 	if isempty(case.loaddata)
-		Pd = -case.bus[:, :Pd]
+		Pd = case.bus[:, :Pd]
 	else
 		Pd[get_load_indices(case)] = case.loaddata.Pd
 	end
 	Pg[get_gen_indices(case)] = case.gen.Pg
-    return Pg + Pd
+    return Pg - Pd
 end
 
 function get_line_lims_pu(case::Case)::Array{Float64}
